a279ba30f79f4a66fb85a873c358bd615d8c7e8b
Donjohn21 <wulver2000@gmail.com>
2024-07-14 03:16:20 -0400

    primera creacion de la pagina principal,cracion del icono principal,remake del logo, creacion del header completo,creacion de la primera fase del body, creacion de 3 botones,3 text box,creacion de todos los archivos

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function toggleSection(element) {
+    var section = element.parentElement; // Obtiene el elemento padre (la sección)
+    var sectionContent = section.querySelector('.section-content'); // Encuentra el contenido de la sección
+
+    // Alterna la visibilidad del contenido de la sección y cambia la imagen
+    if (sectionContent.style.display === 'none' || sectionContent.style.display === '') {
+        sectionContent.style.display = 'block'; // Muestra el contenido
+        element.src = element.getAttribute('data-close-src'); // Cambia la imagen a la de cerrar
+    } else {
+        sectionContent.style.display = 'none'; // Oculta el contenido
+        element.src = element.getAttribute('data-open-src'); // Cambia la imagen a la de abrir
+    }
+
+    // Evita que el botón realice el comportamiento predeterminado de un botón
+    return false;
+}
+
+

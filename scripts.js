df6108499ba568bf692c6b82c09607f8bfa17bbd
Donjohn21 <wulver2000@gmail.com>
2024-07-23 23:23:06 -0400

    agregue el salto de pagina y el bugg de los botones de los textbox

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 function toggleSection(element) {
+    console.log(element.parentElement)
     var section = element.parentElement; // Obtiene el elemento padre (la sección)
     var sectionContent = section.querySelector('.section-content'); // Encuentra el contenido de la sección
 
     // Alterna la visibilidad del contenido de la sección y cambia la imagen
-    if (sectionContent.style.display === 'none' || sectionContent.style.display === '') {
+    if (sectionContent.style.display === 'none') {
         sectionContent.style.display = 'block'; // Muestra el contenido
-        element.src = element.getAttribute('data-close-src'); // Cambia la imagen a la de cerrar
+        element.src = element.getAttribute('data-open-src'); // Cambia la imagen a la de abrir
     } else {
         sectionContent.style.display = 'none'; // Oculta el contenido
-        element.src = element.getAttribute('data-open-src'); // Cambia la imagen a la de abrir
+        element.src = element.getAttribute('data-close-src'); // Cambia la imagen a la de cerrar
     }
 
     // Evita que el botón realice el comportamiento predeterminado de un botón
